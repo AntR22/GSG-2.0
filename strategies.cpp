f21d2bb7a1ec0d41a9fb33befe3231a18a78c9f7
AntR22 <antonragusa0@gmail.com>
2023-02-10 13:14:54 +1100

    strategy implementation for proof of concept

--- strategies.cpp
+++ strategies.cpp
@@ -1,1 +1,17 @@
 #include "strategies.hpp"
+#include "indicators.hpp"
+
+void VWAP (cData &cS, timeProfile &tP, volumeProfile &vP) {
+    double VWAPValue;
+
+    while (true) {
+        if (cS.allCandlesClosed()) {
+            VWAPValue = basicIndicators("VWAP", cS);
+            if (VWAPValue < cS.accessDataAtTime(1).getclosePrice()){
+                std::cout << "buy" << std::endl;
+                cS.printCandlestick(1);
+                cS.printCandlestick(0);
+            }
+        }
+    }
+}
--- strategies.hpp
+++ strategies.hpp
@@ -1,2 +1,4 @@
 #pragma once
+#include "data.hpp"
 
+void VWAP (cData &cS, timeProfile &tP, volumeProfile &vP);
