9dae116c93f452762baea7c57b6b4c739050ec46
AntR22 <antonragusa0@gmail.com>
2023-02-12 21:44:13 +1100

    added example strategies for backtesting + normal

--- strategies.cpp
+++ strategies.cpp
@@ -1,16 +1,37 @@
 #include "strategies.hpp"
 #include "indicators.hpp"
+#include <thread>
+
+// this is an example of how it should be set out for backtesting
+// must enter while loop relativity early in code or else timing between threads is off and lock
+// will be unlocked too early
+void VWAP1 (cData &cS, timeProfile &tP, volumeProfile &vP, std::mutex &m, int &runKey) {
+    double VWAPValue;
+    int counter = -1;
+    while (true) {
+        if (cS.returnStart() != counter) {
+            m.lock();
+            counter = cS.returnStart();
+            VWAPValue = basicIndicators("VWAP", cS, 10);
+            m.unlock();
+        }
+        // sleep for shorter if large workload is performed during if statement
+        // test with high precision clock
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        if (runKey == 1) {
+            return;
+        }
+    }
+}
 
 void VWAP (cData &cS, timeProfile &tP, volumeProfile &vP) {
     double VWAPValue;
-    cS.returnSize();
+    int counter = cS.returnStart();
     while (true) {
-        if (cS.allCandlesClosed()) {
+        if (true) {
             VWAPValue = basicIndicators("VWAP", cS, 10);
-            if (VWAPValue < cS.accessDataAtTime(1).getclosePrice()){
-                std::cout << "cS" << std::endl;
-                cS.printCandlestick(1);
-            }
+            std::cout << VWAPValue << std::endl;
+            return;
         }
     }
 }
--- strategies.hpp
+++ strategies.hpp
@@ -1,4 +1,6 @@
 #pragma once
 #include "data.hpp"
 
+void VWAP1 (cData &cS, timeProfile &tP, volumeProfile &vP, std::mutex &m, int &runKey);
+
 void VWAP (cData &cS, timeProfile &tP, volumeProfile &vP);
